53b061cae596b3e5b79d240f4779368c43b2cd52
shibu <shibu@gogle.com>
2026-02-27 10:38:48 +0530

    forget to commit to last commit

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,45 +1,35 @@
 import { useState } from "react";
+import Menu from "./Menu";
+import AccountLogo from "./AccountLogo";
+import MenuItems from "./MenuItems";
 
 
 export default function SideBar () {
 
-    const menuItems = ["Dashboard", "Sales", "Purchase","Inventory", "Accounts" ,"Payments" ,"Returns" ,"Reports" ];
+    
     
      const [isExpanded, setIsExpanded] = useState(false);
 
     return(
     <>
 
-      {!isExpanded && (
-        <img
-          src="src\assets\menu.svg"
-          alt="menu"
-          className="floating-menu logo"
-          onMouseEnter={() => setIsExpanded(true)}
-          style={{width:'3%'}}
-        
-        />
-      )}
+      
 
 
     <div className={`sidebar Vertical ${isExpanded ? " expanded " : " collapsed "}`}
     onMouseEnter={() => setIsExpanded(true)}
     onMouseLeave={() => setIsExpanded(false)}
     >
-        {isExpanded &&(
-        
-        <div style={{display:'grid',placeItems:'center'}} >
-    
-        <img className="user logo" src="src\assets\image.png" alt="Logo"/>
-        </div>
-        )}
+      {!isExpanded ? (<Menu onExpand={() => setIsExpanded(true)} />)
+        :
+        (<>
+          <AccountLogo/>
+          <MenuItems/>
+          </>
+      
+        )}  
 
-        {isExpanded &&(menuItems.map((item, index) => (
-    
-        <button key={index} className="menu-item">{item}</button>
-        
-      )))}
-    
+       
       
     </div>
 
