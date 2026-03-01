e42f471c81001009f4670be9a62b3f7ab1f31dd4
shibu <shibu@gogle.com>
2026-03-01 11:33:14 +0530

    add table ,scroll ,alternating color

--- src/components/ResultTable.jsx
+++ src/components/ResultTable.jsx
@@ -1,5 +1,52 @@
 export default function(){
+    
+    const invoices = [
+    { id: 1, client: "Client A", amount: 2000 },
+    { id: 2, client: "Client B", amount: 3500 },
+    { id: 3, client: "Client A", amount: 2000 },
+    { id: 4, client: "Client B", amount: 3500 },
+    { id: 5, client: "Client B", amount: 3500 },
+    { id: 6, client: "Client A", amount: 2000 },
+    { id: 7, client: "Client B", amount: 3500 },
+    { id: 8, client: "Client B", amount: 3500 },
+    { id: 9, client: "Client A", amount: 2000 },
+    { id: 10, client: "Client B", amount: 3500 },
+    { id: 11, client: "Client B", amount: 3500 },
+    { id: 12, client: "Client A", amount: 2000 },
+    { id: 13, client: "Client B", amount: 3500 },
+    { id: 2, client: "Client B", amount: 3500 },
+    { id: 1, client: "Client A", amount: 2000 },
+    { id: 2, client: "Client B", amount: 3500 },
+    { id: 2, client: "Client B", amount: 3500 },
+    { id: 1, client: "Client A", amount: 2000 },
+    { id: 2, client: "Client B", amount: 3500 },
+    ];
+    const headers = Object.keys(invoices[0]);
+   
+    
     return(
-        <div className="resultSection"></div>
+<div className="resultSection">
+
+    <div className="tableWrapper">
+        <table className="resultTable">
+          <thead>
+            <tr>
+              {headers.map((key) => (
+                <th key={key}>{key}</th>
+              ))}
+            </tr>
+          </thead>
+          <tbody>
+            {invoices.map((invoice) => (
+              <tr key={invoice.id}>
+                {headers.map((key) => (
+                  <td key={key}>{invoice[key]}</td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </table>
+    </div>
+</div>
     );
 }
