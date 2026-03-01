5af04bd7bb996a47b448edded7edbea0a6bd75ef
shibu <shibu@gogle.com>
2026-03-01 14:33:42 +0530

    make resultsection reusable

--- src/Pages/Sales.jsx
+++ src/Pages/Sales.jsx
@@ -10,6 +10,27 @@ import SearchBox from "../Components/SearchBox";
 export default function(){
     const filterButtons = ["All", "Pending", "Paid"];
     const headerComponents = [AddButton,SearchBox ,PrintButton];
+    const result = [
+    { id: 1, client: "Shibu", amount: 2000 },
+    { id: 2, client: "Client B", amount: 3500 },
+    { id: 3, client: "Client A", amount: 2000 },
+    { id: 4, client: "Client B", amount: 3500 },
+    { id: 5, client: "Client B", amount: 3500 },
+    { id: 6, client: "Client A", amount: 2000 },
+    { id: 7, client: "Client B", amount: 3500 },
+    { id: 8, client: "Client B", amount: 3500 },
+    { id: 9, client: "Client A", amount: 2000 },
+    { id: 10, client: "Client B", amount: 3500 },
+    { id: 11, client: "Client B", amount: 3500 },
+    { id: 12, client: "Client A", amount: 2000 },
+    { id: 13, client: "Client B", amount: 3500 },
+    { id: 14, client: "Client B", amount: 3500 },
+    { id: 15, client: "Client A", amount: 2000 },
+    { id: 16, client: "Client B", amount: 3500 },
+    { id: 17, client: "Client B", amount: 3500 },
+    { id: 18, client: "Client A", amount: 2000 },
+    { id: 19, client: "Client B", amount: 3500 },
+    ];
     return(
         <div className="SearchBarLayout">
            <div className="searchBar">
@@ -18,7 +39,7 @@ export default function(){
             <div className="content">
                  <ContentHeader Components={headerComponents} />
                  <ContentFilter buttons = {filterButtons} />
-                 <ResultTable/>
+                 <ResultTable invoices={result}/>
             
             </div>
             
--- src/components/ResultTable.jsx
+++ src/components/ResultTable.jsx
@@ -1,26 +1,5 @@
-export default function(){
+export default function({invoices}){
     
-    const invoices = [
-    { id: 1, client: "Client A", amount: 2000 },
-    { id: 2, client: "Client B", amount: 3500 },
-    { id: 3, client: "Client A", amount: 2000 },
-    { id: 4, client: "Client B", amount: 3500 },
-    { id: 5, client: "Client B", amount: 3500 },
-    { id: 6, client: "Client A", amount: 2000 },
-    { id: 7, client: "Client B", amount: 3500 },
-    { id: 8, client: "Client B", amount: 3500 },
-    { id: 9, client: "Client A", amount: 2000 },
-    { id: 10, client: "Client B", amount: 3500 },
-    { id: 11, client: "Client B", amount: 3500 },
-    { id: 12, client: "Client A", amount: 2000 },
-    { id: 13, client: "Client B", amount: 3500 },
-    { id: 14, client: "Client B", amount: 3500 },
-    { id: 15, client: "Client A", amount: 2000 },
-    { id: 16, client: "Client B", amount: 3500 },
-    { id: 17, client: "Client B", amount: 3500 },
-    { id: 18, client: "Client A", amount: 2000 },
-    { id: 19, client: "Client B", amount: 3500 },
-    ];
     const headers = Object.keys(invoices[0]);
    
     
