c5a728649bf833cf96b646bef5429b0896d1fe86
Mr BalaMurali <88329928+BaLaamuRaLi@users.noreply.github.com>
2026-03-06 14:54:50 +0530

    add table to selected products

--- src/Pages/SearchProduct.jsx
+++ src/Pages/SearchProduct.jsx
@@ -1,6 +1,8 @@
+import { useMemo } from "react";
 import CloseButton from "../Components/CloseButton";
 import ComponentsExtractor from "../Components/ComponentsExtractor";
 import Input from "../Components/Input";
+import ResultTable from "../Components/ResultTable";
 
 export default function({onClose}){
     const filterConfigs=[
@@ -10,12 +12,46 @@ export default function({onClose}){
     {id:"Batch", Component:Input,type:"text",placeholder:"Batch no:"},
 
     ];
+
+        const result = useMemo(()=>[
+        { id: 1, Product: "apple", Qty: 2000 ,Profit:10},
+        { id: 2, Product: "orange", Qty: 3500 ,Profit:10},
+        { id: 3, Product: "apple", Qty: 2000 ,Profit:10},
+        { id: 4, Product: "apple", Qty: 3500 ,Profit:10},
+        { id: 5, Product: "apple", Qty: 3500 ,Profit:10},
+        { id: 6, Product: "apple", Qty: 2000 ,Profit:10},
+        { id: 7, Product: "apple", Qty: 3500 ,Profit:10},
+        { id: 8, Product: "apple", Qty: 3500 ,Profit:10},
+        { id: 9, Product: "apple", Qty: 2000 ,Profit:10},
+        { id: 10, Product: "apple", Qty: 3500 ,Profit:10},
+        { id: 11, Product: "apple", Qty: 3500 ,Profit:10},
+        { id: 12, Product: "apple", Qty: 2000 ,Profit:10},
+        { id: 13, Product: "apple", Qty: 3500 ,Profit:10},
+        { id: 14, Product: "apple", Qty: 3500 ,Profit:10},
+        { id: 15, Product: "apple", Qty: 2000 ,Profit:10},
+        { id: 16, Product: "apple", Qty: 3500 ,Profit:10},
+        { id: 17, Product: "apple", Qty: 3500 ,Profit:10},
+        { id: 18, Product: "apple", Qty: 2000 ,Profit:10},
+        { id: 19, Product: "apple", Qty: 3500 ,Profit:10},
+        { id: 21, Product: "apple", Qty: 2000 ,Profit:10},
+        { id: 22, Product: "orange", Qty: 3500 ,Profit:10},
+        { id: 23, Product: "apple", Qty: 2000 ,Profit:10},
+        { id: 24, Product: "apple", Qty: 3500 ,Profit:10},
+        { id: 25, Product: "apple", Qty: 3500 ,Profit:10},
+        { id: 26, Product: "apple", Qty: 2000 ,Profit:10},
+        { id: 27, Product: "apple", Qty: 3500 ,Profit:10},
+        { id: 28, Product: "apple", Qty: 3500 ,Profit:10},
+        
+        ],[]);
+    
+
     return(
     <div className="modal center">
         <div className="popup SearchProduct">
             <CloseButton onClick={onClose} />
-            <div className="selectedProducts">
-             <h1>Search Product</h1>
+            <div className="selectedProducts vertical">
+             <ResultTable invoices={result}/>
+
             </div>
             <div className="filterProducts horizontal">
             <ComponentsExtractor components={filterConfigs} />
