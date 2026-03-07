75d117f06c5ebcbce2fc0cea210a94a3ecd1ec6a
Mr BalaMurali <88329928+BaLaamuRaLi@users.noreply.github.com>
2026-03-07 11:55:14 +0530

    add contents to new account page

--- src/Pages/AddAccount.jsx
+++ src/Pages/AddAccount.jsx
@@ -1,11 +1,24 @@
+
 import CloseButton from "../Components/CloseButton";
+import DropBox from "../Components/DropBox";
+import SearchBox from "../Components/SearchBox";
 
 export default function({accountType ,onClose}){
+    const parties =[
+        "Customer","Agent","Supplier"
+    ]
+ 
     return(
     <div className="modal center">
         <div className="popup addAccount">
             <CloseButton onClick={onClose} />
-            <h1>Add {accountType}</h1>
+            <SearchBox placeholder="Party Code"/>
+            <SearchBox placeholder="Name"/>
+            <SearchBox placeholder="GSTIN (if applicable)"/>
+            <SearchBox placeholder="Address"/>
+            <SearchBox placeholder="Phone"/>
+            <DropBox message={"Select Category"} items={parties} name={"category"}
+            value={accountType}/>
         </div>
     </div>
     );
