d70f42428f07d4a55181a7c661338e7d6ece9b99
BaLaamuRaLi <88329928+BaLaamuRaLi@users.noreply.github.com>
2026-03-10 10:05:35 +0530

    change layout of add product

--- src/Pages/AddProduct.jsx
+++ src/Pages/AddProduct.jsx
@@ -11,11 +11,13 @@ export default function({onClose}){
     const batch= {Component:LabelInput,label:"Batch No",type:"text"};
     const taxCategory= {Component:DropBox,label:"Tax" ,items:["18%","5%","40%","New Tax"],message:"--select--" };
     const unit= {Component:DropBox,label:"Unit" ,items:["kg","gram","bag"],message:"--select--" };
-//#endregion
+    
+
+ //#endregion
     const filter1={Component:LabelInput,label:"Type",type:"text",placeholder:"pipe, bulb"}; 
     const filter2={Component:LabelInput,label:"Size",type:"text",placeholder:'9W, 1"'}; 
     const filter3={Component:LabelInput,label:"Company",type:"text",placeholder:'Goldmedal,Supreme'}; 
-    const filter4={Component:LabelInput,label:"Batch no",type:"text"}; 
+    const filter4={Component:LabelInput,label:"filter 4",type:"text"}; 
 
 //#region right
 
@@ -25,14 +27,16 @@ export default function({onClose}){
         <div className="popup addProduct">
         <CloseButton onClick={onClose}/>
 
-        <div className="vertical" style={{gap:"10px"}}>
+        <div className="vertical" style={{gridArea:"1 / 1 / 2 / 2", gap:"10px",padding:"20px 10px",alignItems:"self-start"}}>
             <SidebySide left={code} right={name}/>
             <SidebySide left={hsn} right={batch}/>
-            <SidebySide left={taxCategory} right={unit}/>
-            
+            <SidebySide left={taxCategory} right={unit}/>            
             <LabelInput label="Profit" type="text" />
+            <DropBox label="Expirable" items={["Yes","No"]} message="--select--" />            
+
         </div>
-        <div className="vertical" style={{gap:"10px",alignItems:"center"}}>
+        <div className="vertical"  style={{gridArea:"1 / 2 / 2 / 3", gap:"10px",padding:"20px 0"}}>
+            <h3 style={{margin:"0"}}>Add Filters</h3>
             <SidebySide left={filter1} right={filter2}/>
             <SidebySide left={filter3} right={filter4}/>
         </div>
