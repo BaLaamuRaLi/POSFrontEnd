59741e96d8fdc806f6c1845f8781d473b0164825
BaLaamuRaLi <88329928+BaLaamuRaLi@users.noreply.github.com>
2026-03-12 22:28:53 +0530

    add validation in addAccount

--- src/Pages/AddAccount.jsx
+++ src/Pages/AddAccount.jsx
@@ -1,9 +1,44 @@
 
+import { useState } from "react";
 import CloseButton from "../Components/CloseButton";
 import DropBox from "../Components/DropBox";
 import SearchBox from "../Components/SearchBox";
+import axios from "axios";
 
 export default function({accountType ,onClose}){
+    const[partyCode,setPartyCode]=useState("")
+    const[partyName,setpartyName]=useState("")
+    const[gst,setGST]=useState("")
+    const[address,setAddress]=useState("")
+    const[phone,setPhone]=useState("")
+
+  function  handleClick(){
+    async function addParty () {
+     try{const res= await axios.post('/server/party/add',{
+            type:accountType,
+            party:{partyCode:partyCode,
+            Name:partyName,
+            gst:gst,
+            address:address,
+            phone:phone}
+        })
+        if(res?.data?.sucess){
+        onClose()
+        console.log(res.data.message)
+        }else{
+        console.log(res?.data?.message)
+        }
+    }
+    catch (error) {
+     console.log(error)   
+    }
+
+
+    }
+        addParty()
+        
+    }
+    
     const parties =[
         "Customer","Agent","Supplier"
     ]
@@ -12,15 +47,15 @@ export default function({accountType ,onClose}){
     <div className="modal center">
         <div className="popup addAccount">
             <CloseButton onClick={onClose} />
-            <SearchBox placeholder="Party Code" title="Party Code"/>
-            <SearchBox placeholder="Name" title="Name" />
-            <SearchBox placeholder="GSTIN (if applicable)" title= "GST number"/>
-            <SearchBox placeholder="Address" title="Address" />
-            <SearchBox placeholder="Phone" title="Phone number" />
-            <DropBox message={"Select Category"} items={parties} name={"category"}
-            value={accountType}/>
+            <SearchBox placeholder="Party Code" title="Party Code" onChange={(e)=>setPartyCode(e.target.value)}  value={partyCode} required={true}/>
+            <SearchBox placeholder="Name" title="Name"  onChange={(e)=>setpartyName(e.target.value)}  value={partyName} required={true}/>
+            <SearchBox placeholder="GSTIN (if applicable)" title= "GST number"  onChange={(e)=>setGST(e.target.value)}  value={gst} required={true}/>
+            <SearchBox placeholder="Address" title="Address"  onChange={(e)=>setAddress(e.target.value)}  value={address} required={true}/>
+            <SearchBox placeholder="Phone" title="Phone number"  onChange={(e)=>setPhone(e.target.value)}  value={phone} required={true}/>
+            {!accountType&&(<DropBox message={"Select Category"} items={parties} name={"category"}
+            value={accountType}/>)}
             
-            <button onClick={onClose}
+            <button onClick={handleClick}
             style={{position:"absolute",bottom:"20px",right:"20px"}}
             >Add</button>
         </div>
