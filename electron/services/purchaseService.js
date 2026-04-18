8a03d3048f4e5d721d59942d57552cacf50f9285
BaLaamuRaLi <88329928+BaLaamuRaLi@users.noreply.github.com>
2026-04-18 14:21:32 +0530

    splitting frontend from electron

--- electron/services/purchaseService.js
+++ electron/services/purchaseService.js
@@ -244,7 +244,7 @@ export function createPurchaseService(db){
         });
         const finalBatches=Object.values(groupedBatch);
     
-            
+
             return await savePurchase(db,formattedPurchase,formattedPurchaseItems,finalBatches);
         }
     }
