5d294315bffcab691714c7e2744228c72c5a8def
BaLaamuRaLi <88329928+BaLaamuRaLi@users.noreply.github.com>
2026-04-18 14:54:08 +0530

    remove electron

--- electron/ipc/authIPC.js
+++
@@ -1,7 +0,0 @@
-import { ipcMain } from "electron";
-
-export   function registerAuthHandlers(authService){
-    ipcMain.handle('authentication', async (e,data)=>{
-        return  authService.verifyUser(data);
-    })
-}
